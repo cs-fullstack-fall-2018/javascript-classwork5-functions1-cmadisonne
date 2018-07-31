c184f10468fdacc2be05e03535e8887f303c1726
Lotus <cmadisonne@gmail.com>
2018-07-31 11:37:02 -0500

    Functions1 Input

---
+++ Exercise13.js
@@ -0,0 +1,10 @@
+function printGreeting() {
+    alert("Hello");
+}
+
+function printAudience(){
+    alert("World");
+}
+
+printGreeting();
+printAudience();
---
+++ Exercise14.js
@@ -0,0 +1,11 @@
+function favTeacher(teacherName) {
+    alert(teacherName);
+}
+
+var favTeacherName = "Kevin";
+
+function main(){
+    favTeacher(favTeacherName);
+}
+
+main();
